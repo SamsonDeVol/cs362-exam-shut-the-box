4ddc23816587704d9ad83d78272a73428193d22b
SamsonDeVol <devols@oregonstate.edu>
2022-03-17 19:16:22 -0700

    Add unit test for TileSet method self.generate to return an array of specified length

--- spec/tile_set_spec.rb
+++ spec/tile_set_spec.rb
@@ -9,7 +9,7 @@ describe 'A tile set' do
     end
 
     it 'returns an array of tiles of a specified length' do
-      skip
+      expect(TileSet.generate(10).length()).to eq(10)
     end
 
     it 'returns an array of tiles whose values increase from 1, by 1' do
